f188c8dbf1b5f173db2897ee790977977eda203b
Fai <laukimfairicky@gmail.com>
2021-12-23 14:04:25 +0800

    <WIP>

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var moviedb_1 = require("./moviedb");
-Object.defineProperty(exports, "MovieDb", { enumerable: true, get: function () { return moviedb_1.MovieDb; } });
